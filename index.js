ab68065c5d8dd6cbfcef03c13481a69ed8c5e396
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-20 20:57:05 -0500

    Questions started and showing in console

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
 // TODO: Include packages needed for this application
-
+let inquirer = require("inquirer");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your Repo?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is a description of your Repo?"
+    },
+    {
+        type: "input",
+        name: "table of contents",
+        message: "What is the Table of Contents for your Repo?"
+    },
+    {
+        type: "input",
+        name: "instalation",
+        message: "What are the installation instructions for your Repo?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the useage for your Repo?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "How is your Repo licensed?",
+        choices: ["MIT", "WTFPL", "GPL", "CC", "MPL"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who contributed to your Repo?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What tests are there for your Repo?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What are any questions?"
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("hi");
+    inquirer.prompt(questions).then(function(boo){
+
+        console.table(boo);
+    })
+}
 
 // Function call to initialize app
 init();
